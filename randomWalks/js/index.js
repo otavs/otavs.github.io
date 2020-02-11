8d77c62e36edd918bd20a8be41419bd58d60c960
Otavio Cesar Toma Da Silva <otavioct@amdocs.com>
2020-02-11 11:05:26 -0300

    a

--- randomWalks/js/index.js
+++ randomWalks/js/index.js
@@ -2,15 +2,15 @@ let gui, walkers = [], n = 400, nSteps = 1
 
 function setup() {
 	createCanvas(windowWidth, windowHeight)  
-	createGUI()
-	populateGUI()
+	// createGUI()
+	// populateGUI()
 	for(let i = 0; i < n; i-=-1)
 		walkers.push(new Walker(windowWidth/2, windowHeight/2, 5, randomColor()))
 }
 
 function draw() {
 	update()
-	//  background(255, 255, 255)
+	// background(255, 255, 255)
 	for(let walker of walkers) {
 		for(let i = 0; i < nSteps; i++){
 			walker.step()
