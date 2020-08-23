b78a2c9bbf3f565dd655f7537b495e0e442a6aba
otavs <otavio.cts@gmail.com>
2020-08-23 16:45:18 -0300

    Move lights out to it's own repo

--- lights_out/src/components/Aside.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Aside(props) {
-  return <>
-    <div style={props.style}>
-      <h1>Lights Out!</h1>
-    </div>
-  </>
-}
--- lights_out/src/game/states.js
+++
@@ -1,6 +0,0 @@
-const states = {}
-
-;['WIN', 'PLAY', 'EDIT']
-.forEach((s, i) => states[s] = i)
-
-export default states
